f3b41e354ff8735928f92a0f6930bc3c8e7fc766
Сенин Роман <rastopyr@gmail.com>
2017-05-28 23:15:15 +0300

    [Finish] Fabric.js with webgl

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 /* eslint-env node */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'ember-cli-fabric2-shim'
+  name: 'ember-cli-fabric2-shim',
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    const vendor = this.treePaths.vendor;
+
+    app.import(vendor + '/fabric2.js', { prepend: true });
+    app.import(vendor + '/shims/fabric.js');
+  },
 };
---
+++ tests/unit/utils/fabricjs-import-test.js
@@ -0,0 +1,9 @@
+import fabric from 'fabric';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | fabricjs import');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  assert.ok(fabric);
+});
